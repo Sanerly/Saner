7cef73e612f3422a7bba1d2bbcf62b1db7e027ed
Administrator <sunset3296@163.com>
2018-05-25 18:12:47 +0800

    自定义控件练习——Tab标签

--- app/src/main/java/com/saner/view/measure/SquareLayout.java
+++ app/src/main/java/com/saner/view/measure/SquareLayout.java
@@ -1,5 +1,6 @@
 package com.saner.view.measure;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
@@ -17,7 +18,7 @@ public class SquareLayout extends ViewGroup {
     private int mMaxRow = DEFAULT_MAX_ROW;// 最大行数
     private int mMaxColumn = DEFAULT_MAX_COLUMN;// 最大列数
 
-    private int mOrientation = ORIENTATION_VERTICAL;// 排列方向默认横向
+    private int mOrientation = ORIENTATION_HORIZONTAL;// 排列方向默认横向
 
     public SquareLayout(Context context) {
         this(context, null);
@@ -37,11 +38,12 @@ public class SquareLayout extends ViewGroup {
     private void init(Context context, AttributeSet attrs, int defStyleAttr) {
 
         // 初始化最大行列数
-        mMaxRow = mMaxColumn = 2;
-
+//        mMaxRow = mMaxColumn = 2;
+        mMaxRow = 6;
+        mMaxColumn =3;
     }
 
-
+    @SuppressLint("DrawAllocation")
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 //        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
@@ -57,6 +59,9 @@ public class SquareLayout extends ViewGroup {
 
 
         if (getChildCount() > 0) {
+
+            int[] childWidth = new int[getChildCount()];
+            int[] childHeight = new int[getChildCount()];
 
             for (int i = 0; i < getChildCount(); i++) {
                 View childView = getChildAt(i);
@@ -78,28 +83,141 @@ public class SquareLayout extends ViewGroup {
 
                     MarginLayoutParams params = (MarginLayoutParams) childView.getLayoutParams();
 
-                    int childActualWidth = childView.getMeasuredWidth() + params.leftMargin + params.rightMargin;
-                    int childActualHeight = childView.getMeasuredHeight() + params.topMargin + params.bottomMargin;
+//                    int childActualWidth = childView.getMeasuredWidth() + params.leftMargin + params.rightMargin;
+//                    int childActualHeight = childView.getMeasuredHeight() + params.topMargin + params.bottomMargin;
+//
+//                    if (mOrientation == ORIENTATION_HORIZONTAL) {
+//
+//                        parentDesireWidth += childActualWidth;
+//
+//                        parentDesireHeight = Math.max(parentDesireHeight, childActualHeight);
+//
+//                    } else if (mOrientation == ORIENTATION_VERTICAL) {
+//
+//                        parentDesireHeight += childActualHeight;
+//
+//                        parentDesireWidth = Math.max(parentDesireWidth, childActualWidth);
+//                    }
 
 
-                    if (mOrientation == ORIENTATION_HORIZONTAL) {
+                    childWidth[i] = childView.getMeasuredWidth() + params.topMargin + params.rightMargin;
+                    childHeight[i] = childView.getMeasuredHeight() + params.topMargin + params.bottomMargin;
 
-                        parentDesireWidth += childActualWidth;
-
-                        parentDesireHeight = Math.max(parentDesireHeight, childActualHeight);
-
-                    } else if (mOrientation == ORIENTATION_VERTICAL) {
-
-                        parentDesireHeight += childActualHeight;
-
-                        parentDesireWidth = Math.max(parentDesireWidth, childActualWidth);
-                    }
                     // 合并子元素的测量状态
                     childMeasureState = combineMeasuredStates(childMeasureState, childView.getMeasuredState());
 
                 }
 
             }
+
+            // 声明临时变量存储行/列宽高
+            int indexMultiWidth = 0, indexMultiHeight = 0;
+
+
+            if (mOrientation == ORIENTATION_HORIZONTAL) {
+                //如果子元素数量大于限定值则进行折行计算
+                if (getChildCount() > mMaxColumn) {
+                    // 计算产生的行数
+                    int row = getChildCount() / mMaxColumn;
+                    // 计算余数
+                    int remainder = getChildCount() % mMaxColumn;
+
+                    // 声明临时变量存储子元素宽高数组下标值  
+                    int index = 0;
+
+                    //遍历数组计算父容器期望宽高值
+                    for (int i = 0; i < row; i++) {
+                        for (int j = 0; j < mMaxColumn; j++) {
+                            // 单行宽度累加
+                            indexMultiWidth += childWidth[index];
+                            // 单行高度取最大值
+                            indexMultiHeight = Math.max(indexMultiHeight, childHeight[index++]);
+                        }
+                        // 每一行遍历完后将该行宽度与上一行宽度比较取最大值
+                        parentDesireWidth = Math.max(indexMultiWidth, parentDesireWidth);
+
+                        // 每一行遍历完后累加各行高度
+                        parentDesireHeight += indexMultiHeight;
+
+                        // 重置参数
+                        indexMultiWidth = indexMultiHeight = 0;
+
+                    }
+                    /*
+                     * 如果有余数表示有子元素未能占据一行
+                     */
+                    if (remainder != 0) {
+                             /*
+                         * 遍历剩下的这些子元素将其宽高计算到父容器期望值
+                         */
+                        for (int i = getChildCount() - remainder; i < getChildCount(); i++) {
+                            indexMultiWidth += childWidth[i];
+
+                            indexMultiHeight = Math.max(indexMultiHeight, childHeight[i]);
+
+                        }
+
+                        parentDesireWidth = Math.max(indexMultiWidth, parentDesireWidth);
+                        parentDesireHeight += indexMultiHeight;
+
+                        indexMultiWidth = indexMultiHeight = 0;
+                    }
+
+
+                } else {
+                    //如果子元素数量还没有限制值大那么直接计算即可不须折行
+
+                    for (int i = 0; i < getChildCount(); i++) {
+//                        parentDesireWidth+=childWidth[i];
+//
+//                        parentDesireHeight=Math.max(parentDesireHeight,childHeight[i]);
+
+                        // 累加子元素的实际高度
+                        parentDesireHeight += childHeight[i];
+
+                        // 获取子元素中宽度最大值
+                        parentDesireWidth = Math.max(parentDesireWidth, childWidth[i]);
+                    }
+                }
+
+
+            } else if (mOrientation == ORIENTATION_VERTICAL) {
+                if (getChildCount() > mMaxRow) {
+                    int column = getChildCount() / mMaxRow;
+                    int remainder = getChildCount() % mMaxRow;
+                    int index = 0;
+
+                    for (int x = 0; x < column; x++) {
+                        for (int y = 0; y < mMaxRow; y++) {
+                            indexMultiHeight += childHeight[index];
+                            indexMultiWidth = Math.max(indexMultiWidth, childWidth[index++]);
+                        }
+                        parentDesireHeight = Math.max(parentDesireHeight, indexMultiHeight);
+                        parentDesireWidth += indexMultiWidth;
+                        indexMultiWidth = indexMultiHeight = 0;
+                    }
+
+                    if (remainder != 0) {
+                        for (int i = getChildCount() - remainder; i < getChildCount(); i++) {
+                            indexMultiHeight += childHeight[i];
+                            indexMultiWidth = Math.max(indexMultiHeight, childWidth[i]);
+                        }
+                        parentDesireHeight = Math.max(parentDesireHeight, indexMultiHeight);
+                        parentDesireWidth += indexMultiWidth;
+                        indexMultiWidth = indexMultiHeight = 0;
+                    }
+                } else {
+                    for (int i = 0; i < getChildCount(); i++) {
+                        // 累加子元素的实际宽度
+                        parentDesireWidth += childWidth[i];
+
+                        // 获取子元素中高度最大值
+                        parentDesireHeight = Math.max(parentDesireHeight, childHeight[i]);
+                    }
+                }
+
+            }
+
 
             //考量父容器内边距将其累加到期望值
             parentDesireWidth += getPaddingLeft() + getPaddingRight();
@@ -124,32 +242,96 @@ public class SquareLayout extends ViewGroup {
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
 
-        if (getChildCount()>0){
+        if (getChildCount() > 0) {
             // 声明临时变量存储宽高倍增值
-            int mutil=0;
+            int mutil = 0;
+
+            // 指数倍增值
+            int indexMulti = 1;
+
+            // 声明临时变量存储行/列宽高
+            int indexMultiWidth = 0, indexMultiHeight = 0;
+
+            // 声明临时变量存储行/列临时宽高
+            int tempHeight = 0, tempWidth = 0;
+
 
             for (int i = 0; i < getChildCount(); i++) {
-                View childView=getChildAt(i);
+                View childView = getChildAt(i);
 
-                if (childView.getVisibility()!=GONE){
+                if (childView.getVisibility() != GONE) {
 
-                    MarginLayoutParams params= (MarginLayoutParams) childView.getLayoutParams();
+                    MarginLayoutParams params = (MarginLayoutParams) childView.getLayoutParams();
 
-                    int childActualSize=childView.getMeasuredWidth();
+                    int childActualSize = childView.getMeasuredWidth();
 
 
-                    if (mOrientation==ORIENTATION_HORIZONTAL){
+                    if (mOrientation == ORIENTATION_HORIZONTAL) {
+                          /*
+                         * 如果子元素数量比限定值大
+                         */
+                        if (getChildCount() > mMaxColumn) {
 
-                        childView.layout(getPaddingLeft()+mutil+params.leftMargin,getPaddingTop()+params.topMargin,
-                                getPaddingLeft()+childActualSize+params.leftMargin+mutil,childActualSize+getPaddingTop()+params.topMargin);
+                               /*
+                             * 根据当前子元素进行布局
+                             */
 
-                        mutil+=childActualSize+params.leftMargin+params.rightMargin;
-                    }else if (mOrientation==ORIENTATION_VERTICAL){
+                            if (i < mMaxColumn * indexMulti) {
 
-                        childView.layout(getPaddingLeft()+params.leftMargin,getPaddingTop()+mutil+params.topMargin,
-                                getPaddingLeft()+params.leftMargin+childActualSize,getPaddingTop()+params.topMargin+childActualSize+mutil);
+                                childView.layout(getPaddingLeft() + params.leftMargin + indexMultiWidth, getPaddingTop() + params.topMargin + indexMultiHeight,
+                                        childActualSize + getPaddingLeft() + params.leftMargin + indexMultiWidth, childActualSize + getPaddingTop()
+                                                + params.topMargin + indexMultiHeight);
 
-                        mutil+=childActualSize+params.topMargin+params.bottomMargin;
+
+                                indexMultiWidth += childActualSize + params.leftMargin + params.rightMargin;
+                                tempHeight = Math.max(tempHeight, childActualSize) + params.topMargin + params.bottomMargin;
+                                /*
+                                 * 如果下一次遍历到的子元素下标值大于限定值
+                                 */
+
+                                if (i + 1 >= mMaxColumn * indexMulti) {
+                                    // 那么累加高度到高度倍增值
+                                    indexMultiHeight += tempHeight;
+
+                                    // 重置宽度倍增值
+                                    indexMultiWidth = 0;
+                                    // 增加指数倍增值
+                                    indexMulti++;
+                                }
+
+                            }
+
+
+                        } else {
+                            childView.layout(getPaddingLeft() + mutil + params.leftMargin, getPaddingTop() + params.topMargin,
+                                    getPaddingLeft() + childActualSize + params.leftMargin + mutil, childActualSize + getPaddingTop() + params.topMargin);
+
+                            mutil += childActualSize + params.leftMargin + params.rightMargin;
+                        }
+
+                    } else if (mOrientation == ORIENTATION_VERTICAL) {
+
+                        if (getChildCount() > mMaxRow) {
+                            if (i < mMaxRow * indexMulti) {
+                                childView.layout(getPaddingLeft() + params.leftMargin + indexMultiWidth, getPaddingTop() + params.topMargin + indexMultiHeight,
+                                        childActualSize + getPaddingLeft() + params.leftMargin + indexMultiWidth, childActualSize + getPaddingTop()
+                                                + params.topMargin + indexMultiHeight);
+                                indexMultiHeight += childActualSize + params.topMargin + params.bottomMargin;
+                                tempWidth = Math.max(tempWidth, childActualSize) + params.leftMargin + params.rightMargin;
+                                if (i + 1 >= mMaxRow * indexMulti) {
+                                    indexMultiWidth += tempWidth;
+                                    indexMultiHeight = 0;
+                                    indexMulti++;
+                                }
+                            }
+                        } else {
+                            childView.layout(getPaddingLeft() + params.leftMargin, getPaddingTop() + mutil + params.topMargin,
+                                    getPaddingLeft() + params.leftMargin + childActualSize, getPaddingTop() + params.topMargin + childActualSize + mutil);
+
+                            mutil += childActualSize + params.topMargin + params.bottomMargin;
+                        }
+
+
                     }
 
 
---
+++ app/src/main/java/com/saner/view/measure/TabLabelLayout.java
@@ -0,0 +1,153 @@
+package com.saner.view.measure;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.saner.util.LogUtil;
+
+/**
+ * Created by sunset on 2018/5/25.
+ */
+
+public class TabLabelLayout extends ViewGroup {
+    public TabLabelLayout(Context context) {
+        this(context, null);
+    }
+
+    public TabLabelLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public TabLabelLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+
+
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+//        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        /* 声明临时变量存储父容器的期望值 该值应该等于父容器的内边距加上所有子元素的测量宽高和外边距*/
+        int parentDesireWidth = 0;
+        int parentDesireHeight = 0;
+        int childMeasureState = 0;
+
+        int layoutWidth = getWidth();
+        if (getChildCount() > 0) {
+
+            for (int i = 0; i < getChildCount(); i++) {
+
+                View childView = getChildAt(i);
+
+                if (childView.getVisibility() != GONE) {
+                    measureChildWithMargins(childView, widthMeasureSpec, 0, heightMeasureSpec, 0);
+
+                    int childMeasureSize = Math.max(childView.getMeasuredWidth(), childView.getMeasuredHeight());
+
+                    int childMeasureSpec = MeasureSpec.makeMeasureSpec(childMeasureSize, MeasureSpec.EXACTLY);
+
+                    childView.measure(childMeasureSpec, childMeasureSpec);
+
+                    MarginLayoutParams params = (MarginLayoutParams) childView.getLayoutParams();
+
+                    int childWidth = childView.getMeasuredWidth() + params.leftMargin + params.rightMargin;
+                    int childHeight = childView.getMeasuredHeight() + params.topMargin + params.bottomMargin;
+                    parentDesireHeight = Math.max(parentDesireHeight, childHeight);
+                    if (parentDesireWidth >= layoutWidth) {
+                        parentDesireWidth = 0;
+                        parentDesireHeight += childHeight;
+                    }
+
+                    parentDesireWidth += childWidth;
+
+
+                    childMeasureState = combineMeasuredStates(childMeasureState, childView.getMeasuredState());
+                }
+
+
+            }
+
+            parentDesireWidth += getPaddingLeft() + getPaddingRight();
+            parentDesireHeight += getPaddingTop() + getPaddingBottom();
+
+
+            parentDesireWidth = Math.max(parentDesireWidth, getSuggestedMinimumWidth());
+            parentDesireHeight = Math.max(parentDesireHeight, getSuggestedMinimumHeight());
+
+
+        }
+
+        setMeasuredDimension(resolveSizeAndState(parentDesireWidth, widthMeasureSpec, childMeasureState),
+                resolveSizeAndState(parentDesireHeight, heightMeasureSpec, childMeasureState << MEASURED_HEIGHT_STATE_SHIFT));
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+        int layoutWidth = 0;
+        int layoutHeight = 0;
+
+        if (getChildCount() > 0) {
+            int mutil = 0;
+            for (int i = 0; i < getChildCount(); i++) {
+                View childView = getChildAt(i);
+
+                if (childView.getVisibility() != GONE) {
+
+                    MarginLayoutParams params = (MarginLayoutParams) childView.getLayoutParams();
+
+                    int childWidth = childView.getMeasuredWidth();
+                    int childHeight = childView.getMeasuredHeight();
+                    LogUtil.logd("childHeight =" + childHeight);
+                    LogUtil.logd("layoutHeight 1=" + layoutHeight);
+                    layoutHeight = Math.max(childHeight, layoutHeight);
+                    LogUtil.logd("layoutHeight 2=" + layoutHeight);
+                    if (layoutWidth + childWidth > getWidth()) {
+                        layoutWidth = 0;
+                        layoutHeight += childHeight;
+                    }
+
+
+                    int left = getPaddingLeft() + params.leftMargin + layoutWidth;
+                    int top = getPaddingTop() + params.topMargin + layoutHeight;
+                    int right = getPaddingRight() + getPaddingLeft() + params.leftMargin + params.rightMargin + layoutWidth + childWidth;
+                    int bottom = getPaddingBottom() + getPaddingTop() + params.bottomMargin + params.topMargin + layoutHeight + childHeight;
+
+                    childView.layout(left, top, right, bottom);
+
+                    layoutWidth += childWidth;
+                }
+
+
+            }
+
+
+        }
+
+
+    }
+
+    @Override
+    protected LayoutParams generateDefaultLayoutParams() {
+        return new MarginLayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+    }
+
+    @Override
+    protected LayoutParams generateLayoutParams(LayoutParams p) {
+        return new MarginLayoutParams(p);
+    }
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new MarginLayoutParams(getContext(), attrs);
+    }
+}
